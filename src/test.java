1d2fd355fefe1c9881e591e4126075607f702e08
Oliwia L. Figas <t00199478@365s.ittralee.ie>
2018-09-10 17:20:13 +0100

    Initial commit

---
+++ src/test.java
@@ -0,0 +1,8 @@
+import javax.swing.*;
+
+public class test {
+    public static void main(String[] args) {
+        String name = JOptionPane.showInputDialog("Olivia");
+        JOptionPane.showMessageDialog(null,"Welcome to year 2" +name);
+    }
+}
